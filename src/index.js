55977351be3802379732bacb16098556fd729e9f
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-03-30 14:21:41 -0600

    added the API of the openweathermap site

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ const tempElement = document.querySelector(".temperature p");
 const descElement = document.querySelector(".temperaturedescription p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
+
+const weather = {};
+
+weather.temperature = {
+    unit : 'celcius'
+}
+
+const KELVIN = 273;
+
+const key = "82005d27a116c2880c8f0fcb866998a0";
