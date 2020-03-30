47f88f4525229c5017294b0e8d5b6b515128a8e6
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-03-30 14:15:19 -0600

    added style to the app, is still in process

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('hello');
+const iconElement = document.querySelector(".weatherIcon");
+const tempElement = document.querySelector(".temperature p");
+const descElement = document.querySelector(".temperaturedescription p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
