7cf84b515098a1cc7c10cd66e386093250225968
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-03-30 14:32:00 -0600

    starting the logic of the app, added the position with longitude and latitude

--- src/index.js
+++ src/index.js
@@ -11,5 +11,20 @@ weather.temperature = {
 }
 
 const KELVIN = 273;
-
 const key = "82005d27a116c2880c8f0fcb866998a0";
+
+if('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+
+} else {
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
