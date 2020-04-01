f49ae89425fe7f1dcc385c46a16598fcf44cfeab
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-04-01 16:59:27 -0600

    fixing the linters and browser bug

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-param-reassign, import/prefer-default-export */
 function displayWeatherInfo(iconElement, tempElement, descElement, locationElement, weather) {
-    console.log('test1')
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`; 
-  }
+  iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+  tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
 
-  export {displayWeatherInfo};
+export { displayWeatherInfo };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {displayWeatherInfo} from './functions';
+/* eslint-disable no-use-before-define */
+import { displayWeatherInfo } from './functions';
 
 const iconElement = document.querySelector('.weatherIcon');
 const tempElement = document.querySelector('.temperature p');
@@ -8,10 +9,21 @@ const notificationElement = document.querySelector('.notification');
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-button');
 
+function setPosition(position) {
+  const { latitude } = position.coords;
+  const { longitude } = position.coords;
+  getWeatherAuto(latitude, longitude);
+}
+
+function showError(error) {
+  notificationElement.style.display = 'block';
+  notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
 const weather = {};
 
 weather.temperature = {
-  unit: 'celsius'
+  unit: 'celsius',
 };
 
 const KELVIN = 273;
@@ -25,77 +37,65 @@ if ('geolocation' in navigator) {
   notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
 }
 
-function getWeather(location) {
-  let api = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${key}`;
-
-  fetch(api)
-    .then(function(response) {
-      let data = response.json();
-      return data;
-    })
-    .then(function(data) {
-      weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-      weather.description = data.weather[0].description;
-      weather.iconId = data.weather[0].icon;
-      weather.city = data.name;
-      weather.country = data.sys.country;
-    })
-    .then(function() {
-      displayWeatherInfo(iconElement, tempElement, descElement, locationElement, weather);
-    });
-}
-
-function setPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  getWeatherAuto(latitude, longitude);
-}
-
-function showError(error) {
-  notificationElement.style.display = 'block';
-  notificationElement.innerHTML = `<p> ${error.message} </p>`;
-}
-
-function displayWeatherAuto(){
-  displayWeatherInfo(iconElement, tempElement, descElement, locationElement, weather);
-}
-
 function getWeatherAuto(latitude, longitude) {
-  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  const api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
   fetch(api)
-    .then(function(response) {
-      let data = response.json();
+    .then((response) => {
+      const data = response.json();
       return data;
     })
-    .then(function(data) {
+    .then((data) => {
       weather.temperature.value = Math.floor(data.main.temp - KELVIN);
       weather.description = data.weather[0].description;
       weather.iconId = data.weather[0].icon;
       weather.city = data.name;
       weather.country = data.sys.country;
     })
-    .then(function() {
+    .then(() => {
       displayWeatherAuto();
     });
 }
 
+function getWeather(location) {
+  const api = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${key}`;
+
+  fetch(api)
+    .then((response) => {
+      const data = response.json();
+      return data;
+    })
+    .then((data) => {
+      weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+      weather.description = data.weather[0].description;
+      weather.iconId = data.weather[0].icon;
+      weather.city = data.name;
+      weather.country = data.sys.country;
+    })
+    .then(() => {
+      displayWeatherInfo(iconElement, tempElement, descElement, locationElement, weather);
+    });
+}
+
+function displayWeatherAuto() {
+  displayWeatherInfo(iconElement, tempElement, descElement, locationElement, weather);
+}
+
+
 searchBtn.addEventListener('click', () => {
-  let location = searchInput.value;
-  console.log(location);
+  const location = searchInput.value;
   getWeather(location);
 });
-
 
 
 function celsiusToFahrenheit(temperature) {
   return (temperature * 9) / 5 + 32;
 }
 
-tempElement.addEventListener('click', function() {
+tempElement.addEventListener('click', () => {
   if (weather.temperature.value === undefined) return;
 
-  if (weather.temperature.unit == 'celsius') {
+  if (weather.temperature.unit === 'celsius') {
     let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
     fahrenheit = Math.floor(fahrenheit);
 
